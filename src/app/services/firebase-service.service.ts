47c1770d3257df16ee0c2253606a76e9bb44c26c
Alia Zat <67161403+ALiaZat@users.noreply.github.com>
2021-02-06 22:53:12 +0200

    Firebase service is added

---
+++ src/app/services/firebase-service.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseServiceService {
+
+  constructor(public db: AngularFirestore) {}
+
+  getUsers(){
+    return this.db.collection('users').snapshotChanges();
+  }
+  searchForUser(searchValue: string){
+    return this.db.collection('users',ref => ref.where('nameToSearch', '>=', searchValue)
+      .where('nameToSearch', '<=', searchValue + '\uf8ff'))
+      .snapshotChanges()
+  }
+
+  createUser(value: { name: string; surname: any; age: string; }, avatar: any){
+    return this.db.collection('users').add({
+      name: value.name,
+      nameToSearch: value.name.toLowerCase(),
+      surname: value.surname,
+      age: parseInt(value.age),
+      avatar: avatar
+    });
+  }
+}
